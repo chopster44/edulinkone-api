19e2b8fa316ff26ddc894d3256cba64c401d2f8a
chopster44 <oscar@chopster44.com>
2023-03-12 17:48:39 +0000

    Added jsdoc

--- build/API/edulink.js
+++ build/API/edulink.js
@@ -2,12 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Edulink = void 0;
 const fetchHeaders_1 = require("../types/fetchHeaders");
+/**
+ * Class for interaction with the Edulink API
+ */
 class Edulink {
+    /**
+     * Create an instance of the edulink API helper.
+     * @param {string} schoolId - the part of the web address which goes before .edulinkone.com
+     * @param {string} username - the username used to log in to edulink
+     * @param {string} password - the password used to log in to edulink
+     * @param {number} establishment_id - unknown usage but cannot log in without it
+     */
     constructor(schoolId, username, password, establishment_id) {
         this.isAuthenticated = false;
         this.schoolId = schoolId;
         this.Authenticate({ data: { username, password, establishment_id } });
     }
+    /**
+     * Send a request to the edulink API
+     * @param {Params} params - the edulink action and any data being sent in the http request body
+     * @private
+     */
     async request(params) {
         const response = await fetch(`https://${this.schoolId}.edulinkone.com/api/`, {
             headers: (this.isAuthenticated ? { ...fetchHeaders_1.fetchHeaders, "Authorization": `Bearer ${this.authToken}`, "X-API-Method": `EduLink.${params.action}` } : { ...fetchHeaders_1.fetchHeaders, "X-API-Method": `EduLink.${params.action}` }),
@@ -26,6 +41,12 @@ class Edulink {
             return response;
         }
     }
+    /**
+     * Used to log in to edulink allowing the api to be used.
+     * @param {AuthParams} params - takes the schoolId, user and password
+     * @constructor
+     * @private
+     */
     async Authenticate(params) {
         params.action = "Login";
         // @ts-ignore
@@ -34,34 +55,60 @@ class Edulink {
         this.authToken = response.result.authtoken;
         this.learner_id = response.result.user.id;
     }
+    /**
+     * gets the raw edulink API timetable data
+     * @param {RawTimetableParams} params - takes in the current date and userid/learner_id
+     * @return {RawTimetableResult}
+     */
     async getRawTimetable(params) {
         params.action = "Timetable";
         // @ts-ignore
         return await this.request(params);
     }
+    /**
+     * gets the data for the week edulink classes as the 'current week'
+     * @return {Week}
+     */
     async getThisWeek() {
         let rawTimetable = await this.getRawTimetable({ data: { date: (new Date).toISOString().split('T')[0], learner_id: this.learner_id } });
         return rawTimetable.result.weeks[0];
     }
+    /**
+     * gets the data for the day edulink classes as "today"
+     * @return {Day}
+     */
     async getToday() {
         //@ts-ignore
         let rawWeek = this.getThisWeek();
         for (let i = 0; i < rawWeek.days.length; i++) {
-            if (rawWeek.days[i].date === (new Date).toISOString().split('T')[0]) {
+            if (rawWeek.days[i].is_current === true) {
                 return rawWeek.days[i];
             }
         }
     }
+    /**
+     * gets the raw edulink API homework data
+     * @param {RawHomeworkParams} params - not sure why, but it wants a number here
+     * @return {RawHomeworkResult}
+     */
     async getRawHomework(params) {
         params.action = "Homework";
         // @ts-ignore
         return await this.request(params);
     }
+    /**
+     * gets the homework edulink calls current
+     * @return {HomeworkResult}
+     */
     async getCurrentHomework() {
         //@ts-ignore
         let rawHomework = await this.getRawHomework({ data: { format: 2 } });
         return rawHomework.result.homework.current;
     }
+    /**
+     * gets the homework edulink calls past
+     * @return {HomeworkResult}
+     */
     async getPastHomework() {
         //@ts-ignore
         let rawHomework = await this.getRawHomework({ data: { format: 2 } });
