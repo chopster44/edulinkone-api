f851af55149545b285c2fd84c444892d32ec07d7
chopster44 <oscar@chopster44.com>
2023-03-13 19:54:18 +0000

    Added timetable tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,10 +1,29 @@
-import {Edulink} from "../index.js";
+import {Edulink, EdulinkTypes} from "../index.js";
 console.log(process.argv)
 async function main () {
 	let edulinkTest = new Edulink(process.argv[2], process.argv[3], process.argv[4], 2);
 	await edulinkTest.Authenticate();
 	if (!edulinkTest.isAuthenticated) {
 		throw new Error("Auth Error: class has not authenticated");
+	}
+
+	let rawTimetable: EdulinkTypes.ResultTypes.RawTimetableResult = await edulinkTest.getRawTimetable({data:  {
+			date: (new Date).toISOString().split("T")[0],
+			learner_id: edulinkTest.learner_id
+		}
+	});
+	if (rawTimetable == undefined) {
+		throw new Error("Timetable Error: out from timetable is undefined");
+	}
+
+	let thisWeek = await edulinkTest.getThisWeek();
+	if (thisWeek == undefined) {
+		throw new Error("Timetable Error: out from getThisWeek() is undefined");
+	}
+
+	let today = await edulinkTest.getToday();
+	if (today == undefined) {
+		throw new Error("Timetable Error: out from getToday() is undefined");
 	}
 }
 
